ecddff86e63aea47a5014323aea1e6a3495df3a9
elizaabraham <abraham.eliza@weboniselab.com>
2013-03-19 17:29:38 +0530

    Intial Commit - Project Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyDashboard::Application.config.secret_token = '5bb3fa39a359395ffbca3a0bbac57c694ac8335aa61e770b948abea56315b9c9c907263f5d3a321216bb4dfc619e8c36677b3ff6ad2c4efb8d83765644505ca9'
